b23961283caa058b6d83399a7afaa08fb882b227
Fur1uss <marcelogaetealvarado@gmail.com>
2025-10-04 14:18:28 -0300

    Initial commit: NTBeyonder project with animated particles background

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Home.css'
+import Particles from "../../components/Particles/Particles";
+
+const Home = () =>{
+    return (
+        <div className='home-site'>
+        <Particles
+            particleColors={['ffffff', '#ffffff']}
+            particleCount={200}
+            particleSpread={10}
+            speed={0.1}
+            particleBaseSize={100}
+            moveParticlesOnHover={true}
+            alphaParticles={false}
+            disableRotation={false}
+        />
+        <div className='home-site-content'>
+            <h1><b>START YOUR</b> JOURNEY</h1>
+            <div className='home-site-content-buttons'>
+                <a href="">CONTINUE</a>
+                <p>NTBeyonder made by NTB Team</p>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Home
